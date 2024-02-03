4f6c4e052acabb27932e0e62134bcac96e2c7fc5
BriefBassoon117 <115414619+Nickyangtpe@users.noreply.github.com>
2024-02-03 15:46:03 +0800

    發布

---
+++ Buckshot Roulette Luncher/Buckshot Roulette Luncher/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Buckshot_Roulette_Luncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.reload = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(23, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "啟動遊戲/下載遊戲";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Silver;
+            this.progressBar1.Location = new System.Drawing.Point(15, 32);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(829, 32);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Location = new System.Drawing.Point(-2, 415);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(860, 77);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "預估剩餘時間:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(0, 341);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(409, 74);
+            this.panel2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(278, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "取消下載";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // reload
+            // 
+            this.reload.Enabled = true;
+            this.reload.Tick += new System.EventHandler(this.reload_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-95, -70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1030, 486);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(851, 489);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Buckshot Rouette Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer reload;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Buckshot Roulette Luncher/Buckshot Roulette Luncher/Form1.cs
@@ -0,0 +1,351 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buckshot_Roulette_Luncher
+{
+    public partial class Form1 : Form
+    {
+        bool canlunch = true;
+        //  
+        bool dowmloading = false;
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+
+            string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            // 組合檔案路徑
+            string filePath = Path.Combine(appDataFolder, "ReLoad.data");
+            try
+            {
+                // 檢查檔案是否存在
+                if (File.Exists(filePath))
+                {
+                    // 讀取檔案內容
+                    string content = File.ReadAllText(filePath);
+                    bool isContentTrue = content.Trim() == "true";
+                    if (isContentTrue == true)
+                    {
+                        string filePath2 = System.IO.Path.Combine(yourFolderPath, "Buckshot_Roulette.exe");
+                        if (File.Exists(filePath2))
+                        {
+                            File.Delete(filePath2);
+                        }
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"檔案不存在: {filePath}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"存取檔案時發生錯誤: {ex.Message}");
+            }
+        }
+
+
+
+        static bool IsAdmin()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+        // 以管理员权限重新运行
+        static void RunAsAdmin()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.Verb = "runas"; // 请求管理员权限
+            startInfo.FileName = Process.GetCurrentProcess().MainModule.FileName;
+
+            try
+            {
+                Process.Start(startInfo);
+
+                Application.Exit();
+            }
+            catch
+            {
+                Application.Exit();
+            }
+
+            Application.Exit();
+        }
+
+
+
+
+
+
+
+
+        string yourFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Buckshot Roulette Launcher");
+        string uninstallKeyPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button1.Text = "正在檢查驅動";
+            await Task.Delay(1000);
+            check("{038E31C6-56FA-4A22-AFFB-FD0191290D6C}");
+            check("{E900C16E-16BC-3334-C580-C397ADF00392}");
+            check("{37B8F9C7-03FB-3253-8781-2517C99D7C00}");
+            if (canlunch == true)
+            {
+              
+
+                // 在 AppData 資料夾下創建你的資料夾路徑
+                
+                if (!Directory.Exists(yourFolderPath))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(yourFolderPath);
+                        Console.WriteLine($"資料夾已創建: {yourFolderPath}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"無法創建資料夾: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"資料夾已存在: {yourFolderPath}");
+                }
+                dowmloading = true;
+                progressBar1.Maximum = 100;
+                progressBar1.Minimum = 0;
+                string filePath = System.IO.Path.Combine(yourFolderPath, "Buckshot_Roulette.exe");
+                if (File.Exists(filePath))
+                {
+                    try
+                    {
+
+                        System.Diagnostics.Process.Start(filePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("錯誤" + ex + "\n\n建議重新開啟此應用", "錯誤");
+                        File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ReLoad.data"), "true");
+                    }
+                    button1.Text = "啟動遊戲/下載遊戲";
+                    dowmloading = false;
+                    button1.Enabled = true;
+                }
+                else
+                {
+                    Lunch();
+                    progressBar1.Style = ProgressBarStyle.Blocks;
+                    button1.Text = "正在下載";
+                }
+                   
+                
+            }
+            else
+            {
+                button1.Text = "啟動遊戲/下載遊戲";
+                button1.Enabled = true;
+            }
+        }
+
+        private void check(string targetKeyName)
+        {
+            // 開啟指定路徑的註冊表機碼
+            using (RegistryKey uninstallKey = Registry.LocalMachine.OpenSubKey(uninstallKeyPath))
+            {
+                if (uninstallKey != null)
+                {
+                    // 開啟特定機碼
+                    using (RegistryKey subKey = uninstallKey.OpenSubKey(targetKeyName))
+                    {
+
+                        // 確保機碼存在並包含 DisplayName 屬性
+                        Console.WriteLine($"已找到機碼:{targetKeyName}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("指定的註冊表路徑不存在。");
+                    canlunch = false;
+                    MessageBox.Show("請下載遊戲驅動", "Luncher");
+                }
+            }
+        }
+
+        private async void Lunch()
+        {
+            string downloadUrl = "https://github.com/Nickyangtpe/Buckshot-Roulette-Launcher/releases/download/%E9%81%8A%E6%88%B2%E6%9C%AC%E9%AB%94(%E9%9D%9E%E5%95%9F%E5%8B%95%E5%99%A8)/Buckshot.Roulette.exe";
+            string targetFolder = yourFolderPath;
+
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+
+                    // 設定下載進度事件
+                    webClient.DownloadProgressChanged += (sender, e) => WebClient_DownloadProgressChanged(e);
+
+                    // 下載檔案到目標資料夾 (使用異步方法)
+                    await DownloadFileAsync(webClient, downloadUrl, System.IO.Path.Combine(targetFolder, "Buckshot_Roulette.exe"));
+                }
+
+                Console.WriteLine("下載完成");
+
+                System.Diagnostics.Process.Start(System.IO.Path.Combine(targetFolder, "Buckshot_Roulette.exe"));
+
+                progressBar1.Style = ProgressBarStyle.Marquee;
+            }
+            catch (Exception ex)
+            {
+                // 下載失敗，顯示錯誤訊息
+                MessageBox.Show($"下載時發生錯誤: \n{ex.Message}\n\n\n請稍後重試", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                Application.Exit();
+                Close();
+                // 重置按鈕狀態
+                button1.Text = "啟動遊戲/下載遊戲";
+                button1.Enabled = true;
+
+            }
+        }
+
+
+
+        private async Task DownloadFileAsync(WebClient webClient, string url, string destination)
+        {
+            using (var stream = await webClient.OpenReadTaskAsync(new Uri(url)))
+            using (var fileStream = new System.IO.FileStream(destination, System.IO.FileMode.Create))
+            {
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                long totalBytesRead = 0;
+                long totalBytes = webClient.ResponseHeaders["Content-Length"] != null ? long.Parse(webClient.ResponseHeaders["Content-Length"]) : 0;
+
+                DateTime startTime = DateTime.Now;
+                DateTime lastUpdateTime = DateTime.Now;
+
+                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    if (dowmloading == true)
+                    {
+                        totalBytesRead += bytesRead;
+
+                        // 每次迴圈開頭即時更新進度條
+                        int progressPercentage = (int)((double)totalBytesRead / totalBytes * 100);
+                        UpdateProgressBar(progressPercentage);
+
+                        // 每秒更新一次預估剩餘時間
+                        DateTime currentTime = DateTime.Now;
+                        if ((currentTime - lastUpdateTime).TotalMilliseconds >= 700)
+                        {
+                            lastUpdateTime = currentTime;
+
+                            // 計算剩餘時間
+                            TimeSpan elapsedTime = currentTime - startTime;
+                            TimeSpan remainingTime = TimeSpan.FromSeconds((totalBytes - totalBytesRead) / (totalBytesRead / elapsedTime.TotalSeconds));
+
+                            label1.Text = $"預估剩餘時間: {remainingTime.Hours:D2}:{remainingTime.Minutes:D2}:{remainingTime.Seconds:D2}";
+                        }
+
+                        await fileStream.WriteAsync(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+        private void UpdateProgressBar(int value)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<int>(UpdateProgressBar), value);
+            }
+            else
+            {
+                progressBar1.Value = value;
+            }
+        }
+
+        private void WebClient_DownloadProgressChanged(DownloadProgressChangedEventArgs e)
+        {
+            // 在此可以處理下載進度，例如更新 ProgressBar1
+            int progressPercentage = e.ProgressPercentage;
+            UpdateProgressBar(progressPercentage);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Runi()
+        {
+            if (IsAdmin())
+            {
+                Console.WriteLine("已以管理员权限运行。");
+
+            }
+            else
+            {
+                RunAsAdmin();
+                Hide();
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dowmloading = false;
+            progressBar1.Value = 0;
+            label1.Text = "預估剩餘時間:";
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button1.Text = "啟動遊戲/下載遊戲";
+            InitializeComponent();
+        
+        }
+
+        private void reload_Tick(object sender, EventArgs e)
+        {
+            if (dowmloading == true)
+            {
+                button2.Enabled = true;
+                button1.Enabled = false;
+
+            }
+            else if(dowmloading == false)
+            {
+                progressBar1.Value = 0;
+                label1.Text = "預估剩餘時間:";
+                button1.Enabled = true;
+                button2.Enabled = false;
+            }
+            else
+            {
+                dowmloading = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hide();
+            Runi();
+        }
+    }
+}
